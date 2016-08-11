2b9f2f6cf6deb150cd93d4522f6fb31590af8c93
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-11 14:06:48 +0530

    fix: coverage

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('nsp', function (cb) {
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('generators/**/*.js')
+  return gulp.src('generators/**/index.js')
     .pipe(excludeGitignore())
     .pipe(istanbul({
       includeUntested: true
